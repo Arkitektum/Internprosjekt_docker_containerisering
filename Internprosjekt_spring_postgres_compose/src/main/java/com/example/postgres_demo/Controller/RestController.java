4525430c4e6553a5214252e9cb0d376467c1dacb
Per Ola Stålberg <olastaalberg@gmail.com>
2023-03-30 14:35:21 +0200

    Java, Docker and gradle files

---
+++ Internprosjekt_spring_postgres_compose/src/main/java/com/example/postgres_demo/Controller/RestController.java
@@ -0,0 +1,39 @@
+package com.example.postgres_demo.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.postgres_demo.Service.RestService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+@Controller
+@RequestMapping(path = "/baerekraft")
+@ResponseBody
+public class RestController {
+    
+
+    @Autowired
+    public RestController(RestService restService) {
+    }
+    @RequestMapping(value = "getRegistration", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public ResponseEntity<String> onGetRequest(@RequestParam(value = "id") String id){
+        
+        return ResponseEntity.ok("");
+    }
+
+    @RequestMapping(value = "postRegistration", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+    public ResponseEntity<String> onPostRequest(@RequestBody String body) throws JsonMappingException, JsonProcessingException{
+
+        
+
+        return ResponseEntity.ok("");
+    }
+}
